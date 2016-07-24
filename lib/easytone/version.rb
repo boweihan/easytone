d7e4dbf96731b7ad7d7319c348dc0f96f7d1707e
bowei <bowei.han100@gmail.com>
2016-07-24 16:18:48 -0400

    v0.1.4

--- lib/easytone/version.rb
+++ lib/easytone/version.rb
@@ -1,3 +1,3 @@
 module Easytone
-  VERSION = "0.1.3"
+  VERSION = "0.1.4"
 end
--- lib/tone.rb
+++
@@ -1,7 +0,0 @@
-class ToneClass
-    def self.tone(username, password, text, version = '2016-05-19')
-    tone_url = "https://#{username}:#{password}@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=#{version}&text=#{text}"
-    response = JSON.parse(RestClient.get(tone_url))
-    response["document_tone"]["tone_categories"][0]["tones"]
-  end
-end
