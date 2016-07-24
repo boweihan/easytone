80d3ceeeb94ecbc61c8f6ce75cc674ef2b841fe8
bowei <bowei.han100@gmail.com>
2016-07-24 15:46:40 -0400

    change dependencies

--- lib/easytone.rb
+++ lib/easytone.rb
@@ -1,8 +1,8 @@
 require "easytone/version"
 
 module Easytone
-  def self.tone(username, password, text, version = '2016-05-19')
-    tone_url = "https://#{username_tone}:#{password_tone}@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=#{version}&text=#{text}"
+  def Easytone.tone(username, password, text, version = '2016-05-19')
+    tone_url = "https://#{username}:#{password}@gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=#{version}&text=#{text}"
     response = JSON.parse(RestClient.get(tone_url))
     return response["document_tone"]["tone_categories"][0]["tones"]
   end
---
+++ lib/test.rb
@@ -0,0 +1,3 @@
+include './lib/easytone.rb'
+
+puts Easytone.tone
